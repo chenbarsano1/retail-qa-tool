ebf3d2e2ab59f44ca96e3b1fd0d85d2e7e5680e1
Chen Barsano <chenbarsano@gmail.com>
2025-11-03 20:16:20 +0200

    improved few_shots

--- few_shots.py
+++ few_shots.py
@@ -2,26 +2,26 @@ few_shots = [
     {'Question' : "How many t-shirts do we have left for Nike in XS size and white color?",
      'SQLQuery' : "SELECT sum(stock_quantity) FROM t_shirts WHERE brand = 'Nike' AND color = 'White' AND size = 'XS'",
      'SQLResult': "Result of the SQL query",
-     'Answer' : "91"},
+     'Answer' : "There are 91 Nike t-shirts left in size XS and white color."},
     {'Question': "How much is the total price of the inventory for all S-size t-shirts?",
      'SQLQuery':"SELECT SUM(price*stock_quantity) FROM t_shirts WHERE size = 'S'",
      'SQLResult': "Result of the SQL query",
-     'Answer': "22292"},
+     'Answer': "The total value of all S-size t-shirts in inventory is $22,292."},
     {'Question': "If we have to sell all the Levi’s T-shirts today with discounts applied. How much revenue  our store will generate (post discounts)?" ,
      'SQLQuery' : """SELECT sum(a.total_amount * ((100-COALESCE(discounts.pct_discount,0))/100)) as total_revenue from
 (select sum(price*stock_quantity) as total_amount, t_shirt_id from t_shirts where brand = 'Levi'
 group by t_shirt_id) a left join discounts on a.t_shirt_id = discounts.t_shirt_id
  """,
      'SQLResult': "Result of the SQL query",
-     'Answer': "16725.4"} ,
+     'Answer': "If we sell all Levi’s T-shirts today with discounts applied, the store will generate $16,725.40 in revenue."} ,
      {'Question' : "If we have to sell all the Levi’s T-shirts today. How much revenue our store will generate without discount?" ,
       'SQLQuery': "SELECT SUM(price * stock_quantity) FROM t_shirts WHERE brand = 'Levi'",
       'SQLResult': "Result of the SQL query",
-      'Answer' : "17462"},
+      'Answer' : "If we sell all Levi’s T-shirts today without any discounts, the total revenue will be $17,462."},
     {'Question': "How many white color Levi's shirt I have?",
      'SQLQuery' : "SELECT sum(stock_quantity) FROM t_shirts WHERE brand = 'Levi' AND color = 'White'",
      'SQLResult': "Result of the SQL query",
-     'Answer' : "290"
+     'Answer' : "There are 290 white Levi's shirts in stock."
      },
     {'Question': "how much sales amount will be generated if we sell all large size t shirts today in nike brand after discounts?",
      'SQLQuery' : """SELECT sum(a.total_amount * ((100-COALESCE(discounts.pct_discount,0))/100)) as total_revenue from
@@ -29,6 +29,6 @@ group by t_shirt_id) a left join discounts on a.t_shirt_id = discounts.t_shirt_i
 group by t_shirt_id) a left join discounts on a.t_shirt_id = discounts.t_shirt_id
  """,
      'SQLResult': "Result of the SQL query",
-     'Answer' : "290"
+     'Answer' : "If we sell all large size Nike t-shirts today with discounts applied, the store will generate $290 in revenue."
     }
 ]
--- main.py
+++ main.py
@@ -6,6 +6,20 @@ st.title("T-Shirts: Database Q&A 👕")
 
 question = st.text_input("Ask a question about the database:")
 
+
+def extract_text_from_response(response):
+    last_msg = response["messages"][-1].content
+    
+    if isinstance(last_msg, list) and len(last_msg) > 0 and isinstance(last_msg[0], dict):
+        return last_msg[0].get("text", str(last_msg))
+    
+    elif isinstance(last_msg, dict):
+        return last_msg.get("text", str(last_msg))
+    
+    return str(last_msg)
+
+
+
 if question:
     with st.spinner("🤔 Thinking... please wait"):
         agent = get_few_shot_sql_agent()
@@ -14,13 +28,21 @@ if question:
             {"messages": [{"role": "user", "content": question}]}
         )
 
-        final_answer = response["messages"][-1].content
+        # final_answer = response["messages"][-1].content
+        final_answer = extract_text_from_response(response)
 
         sql_query = None
         for msg in response["messages"]:
-            if isinstance(msg.content, str) and "SELECT" in msg.content.upper():
-                sql_query = msg.content
+            content = msg.content
+            if isinstance(content, str) and "SELECT" in content.upper():
+                sql_query = content
                 break
+
+            elif isinstance(content, list) and len(content) > 0 and isinstance(content[0], dict):
+                text = content[0].get("text", "")
+                if "SELECT" in text.upper():
+                    sql_query = text
+                    break
 
     st.subheader("Answer:")
     st.success(final_answer)
